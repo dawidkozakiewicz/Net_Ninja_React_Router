b02013548aa32e8aa6b589ce30eb175557495448
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-07-20 20:33:23 +0200

    add hoc component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 const Navbar = () => {
     return (
@@ -7,9 +8,9 @@ const Navbar = () => {
                 <a className="brand-logo">Poke'Times</a>
                 <ul className="right">
 
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><NavLink to="/contact">Contact</NavLink></li>
 
                 </ul>
             </div>
---
+++ src/hoc/Rainbow.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Rainbow = (WrappedComponent) => {
+    const colours = ['orange', 'green', 'blue', 'black', 'yellow', 'cyan']
+    const randomColour = colours[Math.floor(Math.random() * 6)]
+    console.log(randomColour)
+    const className = randomColour + '-text'
+
+    return (props) => {
+        console.log(props)
+        return (
+            <div className={className}>
+                <WrappedComponent {...props} />
+            </div>
+        )
+    }
+}
+
+export default Rainbow
